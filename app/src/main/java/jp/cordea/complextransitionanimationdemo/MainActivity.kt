bfee69565709e9aaa044ec90ee3608bdad5c5050
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-07 18:16:37 +0900

    switch fragment

--- app/src/main/java/jp/cordea/complextransitionanimationdemo/MainActivity.kt
+++ app/src/main/java/jp/cordea/complextransitionanimationdemo/MainActivity.kt
@@ -1,12 +1,34 @@
 package jp.cordea.complextransitionanimationdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var isMain: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, MainFragment.newInstance())
+            .commit()
+
+        fab.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.container,
+                    if (isMain) {
+                        SubFragment.newInstance()
+                    } else {
+                        MainFragment.newInstance()
+                    }
+                )
+                .commit()
+            isMain = !isMain
+        }
     }
 }
